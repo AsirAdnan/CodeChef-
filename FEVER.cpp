1073004478484a6d10429bfe58f4c635e0724f5b
Asir Adnan <106885381+AsirAdnan@users.noreply.github.com>
2022-09-14 06:24:37 +0600

    Create FEVER.cpp

---
+++ FEVER.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cin>>a;
+    for (int i=0;i<a;i++){
+        cin>>b;
+        if (b>98){
+            cout<<"YES\n";
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+}
